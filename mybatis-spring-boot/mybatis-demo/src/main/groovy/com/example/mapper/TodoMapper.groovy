772a231e0c5daf51f50c64b9b1429a440b7e7876
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-07-13 00:34:06 +0900

    Support Groovy for MyBatis Mapper interface

---
+++ mybatis-spring-boot/mybatis-demo/src/main/groovy/com/example/mapper/TodoMapper.groovy
@@ -0,0 +1,31 @@
+package com.example.mapper
+
+import com.example.domain.Todo
+import org.apache.ibatis.annotations.Insert
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Options
+import org.apache.ibatis.annotations.Select
+
+@Mapper
+interface TodoMapper {
+
+    @Insert('''
+        INSERT INTO todo
+            (title, details, finished)
+        VALUES
+            (#{title}, #{details}, #{finished})
+    ''')
+    @Options(useGeneratedKeys = true)
+    void insert(Todo todo);
+
+    @Select('''
+        SELECT
+            id, title, details, finished
+        FROM
+            todo
+        WHERE
+            id = #{id}
+    ''')
+    Todo select(int id);
+
+}
