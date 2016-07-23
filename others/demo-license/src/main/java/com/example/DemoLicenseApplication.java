305e685ab25990063879f0f5860d3d0d813e4b89
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-07-23 12:27:58 +0900

    Add html file

--- others/demo-license/src/main/java/com/example/DemoLicenseApplication.java
+++ others/demo-license/src/main/java/com/example/DemoLicenseApplication.java
@@ -2,11 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoLicenseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoLicenseApplication.class, args);
 	}
+
+
+	@RequestMapping("/")
+	String home() {
+		return "home";
+	}
+
 }
