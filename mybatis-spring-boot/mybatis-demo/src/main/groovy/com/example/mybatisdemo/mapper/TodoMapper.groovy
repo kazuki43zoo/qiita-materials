ab865d35a7f8dda3c256057a6bfda32385633b0a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2019-05-03 15:46:13 +0900

    Apply mybatis-spring-boot 2.0.1

---
+++ mybatis-spring-boot/mybatis-demo/src/main/groovy/com/example/mybatisdemo/mapper/TodoMapper.groovy
@@ -0,0 +1,31 @@
+package com.example.mybatisdemo.mapper
+
+import com.example.mybatisdemo.domain.Todo
+import org.apache.ibatis.annotations.Insert
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Options
+import org.apache.ibatis.annotations.Select
+
+@Mapper
+interface TodoMapper {
+  
+  @Insert('''
+        INSERT INTO todo
+            (title, details, finished)
+        VALUES
+            (#{title}, #{details}, #{finished})
+    ''')
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void insert(Todo todo)
+  
+  @Select('''
+        SELECT
+            id, title, details, finished
+        FROM
+            todo
+        WHERE
+            id = #{id}
+    ''')
+  Todo select(int id)
+  
+}
